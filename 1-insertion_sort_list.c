1f0bd82deea8b2cf30cd9e50f40abde1e1b1fe7a
Collinb190 <collinb190@gmail.com>
2023-12-18 00:01:34 -0600

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - Sorts a doubly linked list of ints in ascending order
  *                        using the Insertion sort algorithm
  *
  * @list: Pointer to the head of the doubly linked list
